712a11eb0b3166fd06966bf926ea920612fcbf05
Abdulmalik Yermekbay <malikdemaximus@users.noreply.github.com>
2025-05-15 15:29:59 +0500

    Create login_page.py

---
+++ ui-testing/selenium-python/pages/login_page.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_field = (By.ID, "user-name")
+        self.password_field = (By.ID, "password")
+    
+    def login(self, username, password):
+        self.driver.find_element(*self.username_field).send_keys(username)
+        self.driver.find_element(*self.password_field).send_keys(password)
+        self.driver.find_element(By.ID, "login-button").click()
