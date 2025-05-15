db189d9958d29fbfebc07543d70ebe5d319b06da
Abdulmalik Yermekbay <malikdemaximus@users.noreply.github.com>
2025-05-15 15:30:27 +0500

    Update test_login.py

--- ui-testing/selenium-python/test_login.py
+++ ui-testing/selenium-python/test_login.py
@@ -1,13 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
 
-def test_successful_login():
+def test_login():
     driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com/")
-    
-    driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.find_element(By.ID, "login-button").click()
-    
+    login_page = LoginPage(driver)
+    login_page.login("standard_user", "secret_sauce")
     assert "inventory" in driver.current_url
-    driver.quit()
